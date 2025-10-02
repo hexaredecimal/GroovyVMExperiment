1d715decb0bc00ef29b67637901a70f0d9b5da06
hexaredecimal <mfanakagama@gmail.com>
2025-10-02 20:32:09 +0200

    ScriptLoader: Load and execute multiple scripts from a directory

--- app/src/main/groovy/vm/ScriptLoader.groovy
+++ app/src/main/groovy/vm/ScriptLoader.groovy
@@ -1,13 +1,22 @@
+package vm;
+
 import groovy.lang.GroovyShell;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import javax.swing.SwingUtilities;
+import java.util.Arrays;
 
 public class ScriptLoader {
+    private static GroovyShell shell = new GroovyShell();
+
     public static void execute(String code) {
-        GroovyShell shell = new GroovyShell();
-        shell.evaluate(code);
+
+        synchronized (shell) {
+            shell.evaluate(code);
+        }
+
     }
 
     public static void executeScript(String path) {
@@ -15,11 +24,30 @@ public class ScriptLoader {
         if (code == null) {
             return;
         }
-
-
-
         execute(code);
     }
+
+    public static void loadScripts(String path) {
+        File dir = new File(path);
+
+        if (!dir.exists() || !dir.isDirectory()) {
+            return;
+        }
+
+        File[] groovyFiles = dir.listFiles((d, name) -> name.endsWith(".groovy"));
+
+        if (groovyFiles == null || groovyFiles.length == 0) {
+            return;
+        }
+
+        Arrays.sort(groovyFiles, (f1, f2) -> f1.getName().compareToIgnoreCase(f2.getName()));
+        for (File file : groovyFiles) {
+            String scriptPath = file.getAbsolutePath();
+            System.out.println("RUNNING: " + scriptPath + "\n\n");
+            executeScript(scriptPath);
+        }
+    }
+
 
 	public static String readFile(String path) {
 		try {
