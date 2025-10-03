43a8b38bbce17e586532161ff2268ebbba5c2516
hexaredecimal <mfanakagama@gmail.com>
2025-10-03 17:26:53 +0200

    VMWindow: Add all the required code for menu switching

--- ui/src/main/groovy/vm/ui/VMWindow.groovy
+++ ui/src/main/groovy/vm/ui/VMWindow.groovy
@@ -16,6 +16,13 @@ import java.lang.Math;
 import vm.ui.VirtualMachine;
 import java.util.Random;
 import vm.ui.VMDesktop;
+import javax.swing.JMenuItem;
+import javax.swing.JSeparator;
+import javax.swing.event.InternalFrameEvent;
+import javax.swing.event.InternalFrameListener;
+import vm.ui.Utils;
+import javax.swing.plaf.basic.BasicInternalFrameUI;
+import javax.swing.JComponent
 
 public class VMWindow extends JInternalFrame {
     private List<JMenu> menus; 
@@ -28,11 +35,91 @@ public class VMWindow extends JInternalFrame {
 
         Random rand = new Random();
         Dimension dSize = vm.getSize();
-        int x = rand.nextInt(Math.max(1, dSize.width / 2 + dSize.width / 4) as Integer);
-        int y = rand.nextInt(Math.max(1, dSize.height / 2 + dSize.height / 4) as Integer);
+        int x = rand.nextInt(Math.max(1, dSize.width / 2 - dSize.width / 4) as Integer);
+        int y = rand.nextInt(Math.max(1, dSize.height / 2 - dSize.height / 4) as Integer);
         setLocation(x, y);
 
         toFront();
+
+        JMenu appWindowCloseMenu = new JMenu(title);
+        JMenuItem minimize = new JMenuItem("Minimize Window");
+        JMenuItem maximize = new JMenuItem("Maximize Window");
+        JMenuItem close = new JMenuItem("Close Window");
+
+        appWindowCloseMenu.add(minimize);
+        appWindowCloseMenu.add(maximize);
+        appWindowCloseMenu.add(new JSeparator());
+        appWindowCloseMenu.add(close);
+
+        menus.add(appWindowCloseMenu);
+
+        minimize.addActionListener { event -> 
+            this.setIcon(true);
+        }
+
+        maximize.addActionListener { event -> 
+            this.setMaximum(true);
+        }
+
+        close.addActionListener { event -> 
+            this.setClosed(true);
+        }
+
+        VMDesktop desktop = vm.getDesktop();
+
+        addInternalFrameListener(new InternalFrameListener() {
+            @Override
+            public void internalFrameActivated(InternalFrameEvent ev) {
+                if (!menus.isEmpty())
+                    vm.useMenus(menus);
+            }
+
+            @Override
+            public void internalFrameDeactivated(InternalFrameEvent ev) {
+                if (desktop.getSelectedFrame() == null) {
+                    vm.useSystemMenus();
+                }
+            }
+
+            @Override
+            public void internalFrameOpened(InternalFrameEvent ev) {
+                if (!menus.isEmpty())
+                    vm.useMenus(menus);
+            }
+
+            public void internalFrameClosing(InternalFrameEvent ev) {}
+            
+            @Override
+            public void internalFrameClosed(InternalFrameEvent ev) {
+                if (desktop.getSelectedFrame() == null) {
+                    vm.useSystemMenus();
+                }
+            }
+            
+            @Override
+            public void internalFrameIconified(InternalFrameEvent ev) {
+                vm.useSystemMenus();
+            }
+
+            @Override
+            public void internalFrameDeiconified(InternalFrameEvent ev) {
+                if (!menus.isEmpty())
+                    vm.useMenus(menus);
+            }
+        });
+
+        Utils.addClickEvent(this, {item -> 
+            if (!menus.isEmpty())
+               vm.useMenus(menus);
+        })
+
+        BasicInternalFrameUI ui = (BasicInternalFrameUI) this.getUI();
+        JComponent titleBar = ui.getNorthPane();
+        
+        Utils.addClickEvent(titleBar, {item -> 
+            if (!menus.isEmpty())
+               vm.useMenus(menus);
+        })
 
         try {
             setSelected(true);
