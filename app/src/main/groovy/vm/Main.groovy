28c2433308a6a7a70b226e5b4ecf45b2619a49c5
hexaredecimal <mfanakagama@gmail.com>
2025-10-03 09:25:28 +0200

    Main: Move the VM to the runtime. Now Main only loads init scripts

--- app/src/main/groovy/vm/Main.groovy
+++ app/src/main/groovy/vm/Main.groovy
@@ -1,21 +1,11 @@
 
 package vm;
 
-import java.lang.System;
-
-import vm.ui.VirtualMachine;
-import vm.ui.VMWindow;
-import vm.ui.VMDesktop
 import vm.ScriptLoader
-
-import java.io.File;
 
 
 public class Main {
     public static void main(args) {
-        VirtualMachine vm = VirtualMachine.getInstance();
-        vm.run();
-        
         ScriptLoader.loadScripts("../vm/init");
     }
 }
